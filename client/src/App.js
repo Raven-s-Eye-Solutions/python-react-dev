a61d6fb606be5ee21689fbf10528f829c9b557e2
Karsten Rabe <ravenseyesolutions@gmail.com>
2020-08-18 09:48:54 -0400

    Handle fetch errors

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,21 @@ import Header from './Components/Header';
 
 export const App = () => {
   const [time, setTime] = useState(null);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     async function fetchTime() {
       const response = await fetch('/current-time');
-      const data = await response.json();
-      if (data.time) {
-        const date = new Date();
-        date.setTime(data.time * 1000);
-        date.toUTCString();
-        setTime(date);
+      if (response.ok) {
+        const data = await response.json();
+        if (data.time) {
+          const date = new Date();
+          date.setTime(data.time * 1000);
+          date.toUTCString();
+          setTime(date);
+        }
+      } else {
+        setError(response.statusText);
       }
     }
     fetchTime();
@@ -22,7 +27,13 @@ export const App = () => {
   return (
     <div className='App'>
       <Header />
-      <p className='pt-2 text-center'>{!time ? 'Loading...' : `Server time: ${time}`}</p>
+      {!error ? (
+        <p className='pt-2 text-center text-success'>
+          {!time ? 'Loading...' : `Server time: ${time}`}
+        </p>
+      ) : (
+        <p className='pt-2 text-center text-error'>{error}</p>
+      )}
     </div>
   );
 };
